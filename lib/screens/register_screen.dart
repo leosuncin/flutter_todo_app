d7daf3e96c137d21226918011590d5faad6c416d
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-08-12 22:41:49 -0600

    Fix scroll of register screen
    Disable autovalidate

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -84,107 +84,105 @@ class _RegisterScreenState extends State<RegisterScreen> {
       key: _screen,
       body: Center(
         child: SingleChildScrollView(
-          child: Container(
+          child: Card(
             margin: mq.orientation == Orientation.portrait
                 ? EdgeInsets.all(mq.size.width / 6)
-                : EdgeInsets.symmetric(horizontal: mq.size.width / 3),
-            height: 400,
-            child: Card(
+                : EdgeInsets.symmetric(horizontal: mq.size.width / 4),
+            child: Form(
+              key: _form,
               child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: Form(
-                  key: _form,
-                  autovalidate: true,
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        decoration: InputDecoration(
-                          labelText: 'Name',
-                        ),
-                        autofocus: true,
-                        textInputAction: TextInputAction.next,
-                        onFieldSubmitted: (_) {
-                          FocusScope.of(context).requestFocus(_emailFocusNode);
-                        },
-                        validator: (value) {
-                          if (value == null || value.isEmpty)
-                            return 'Name should not be empty';
+                padding: const EdgeInsets.only(
+                  right: 20,
+                  bottom: 20,
+                  left: 20,
+                ),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Name',
+                      ),
+                      autofocus: true,
+                      textInputAction: TextInputAction.next,
+                      onFieldSubmitted: (_) {
+                        FocusScope.of(context).requestFocus(_emailFocusNode);
+                      },
+                      validator: (value) {
+                        if (value == null || value.isEmpty)
+                          return 'Name should not be empty';
 
-                          return null;
-                        },
-                        onSaved: (value) {
-                          setState(() {
-                            name = value;
-                          });
-                        },
+                        return null;
+                      },
+                      onSaved: (value) {
+                        setState(() {
+                          name = value;
+                        });
+                      },
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Email',
                       ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          labelText: 'Email',
-                        ),
-                        textInputAction: TextInputAction.next,
-                        keyboardType: TextInputType.emailAddress,
-                        focusNode: _emailFocusNode,
-                        onFieldSubmitted: (_) {
-                          FocusScope.of(context)
-                              .requestFocus(_passwordFocusNode);
-                        },
-                        validator: (value) {
-                          if (value == null || value.isEmpty)
-                            return 'Email should not be empty';
+                      textInputAction: TextInputAction.next,
+                      keyboardType: TextInputType.emailAddress,
+                      focusNode: _emailFocusNode,
+                      onFieldSubmitted: (_) {
+                        FocusScope.of(context).requestFocus(_passwordFocusNode);
+                      },
+                      validator: (value) {
+                        if (value == null || value.isEmpty)
+                          return 'Email should not be empty';
 
-                          return null;
-                        },
-                        onSaved: (value) {
-                          setState(() {
-                            email = value;
-                          });
-                        },
+                        return null;
+                      },
+                      onSaved: (value) {
+                        setState(() {
+                          email = value;
+                        });
+                      },
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Password',
                       ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          labelText: 'Password',
-                        ),
-                        obscureText: true,
-                        textInputAction: TextInputAction.done,
-                        focusNode: _passwordFocusNode,
-                        onFieldSubmitted: (_) => _submit(),
-                        validator: (value) {
-                          if (value == null || value.isEmpty)
-                            return 'Password should not be empty';
+                      obscureText: true,
+                      textInputAction: TextInputAction.done,
+                      focusNode: _passwordFocusNode,
+                      onFieldSubmitted: (_) => _submit(),
+                      validator: (value) {
+                        if (value == null || value.isEmpty)
+                          return 'Password should not be empty';
 
-                          if (value.length < 8)
-                            return 'Password too short (at least 8 characters required)';
+                        if (value.length < 8)
+                          return 'Password too short (at least 8 characters required)';
 
-                          return null;
-                        },
-                        onSaved: (value) {
-                          setState(() {
-                            password = value;
-                          });
-                        },
-                      ),
-                      SizedBox.fromSize(
-                        size: Size(double.infinity, 30),
-                      ),
-                      PrimaryButton(
-                        text: 'Sign up',
-                        onPressed: _submit,
-                      ),
-                      SecondaryButton(
-                        text: 'Sign in',
-                        onPressed: () {
-                          if (Navigator.of(context).canPop()) {
-                            Navigator.of(context).pop();
-                          } else {
-                            Navigator.of(context)
-                                .pushNamed(LoginScreen.routeName);
-                          }
-                        },
-                      ),
-                    ],
-                  ),
+                        return null;
+                      },
+                      onSaved: (value) {
+                        setState(() {
+                          password = value;
+                        });
+                      },
+                    ),
+                    SizedBox.fromSize(
+                      size: Size(double.infinity, 30),
+                    ),
+                    PrimaryButton(
+                      text: 'Sign up',
+                      onPressed: _submit,
+                    ),
+                    SecondaryButton(
+                      text: 'Sign in',
+                      onPressed: () {
+                        if (Navigator.of(context).canPop()) {
+                          Navigator.of(context).pop();
+                        } else {
+                          Navigator.of(context)
+                              .pushNamed(LoginScreen.routeName);
+                        }
+                      },
+                    ),
+                  ],
                 ),
               ),
             ),
